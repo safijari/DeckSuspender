69f226ae382db6f22b13bfea8197a1ee8ad42345
Jariullah <safijari@isu.edu>
2022-06-22 00:53:53 -0500

    Explicit options

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import os
+import time
+
+# try:
+#     os.unlink("/tmp/report")
+# except Exception:
+#     pass
+
+# def indicate(msg):
+#     with open("/tmp/report", "a") as f:
+#         f.write(str(time.time()) + " " + msg + "\n")
 import sys
+
 sys.path.append("/home/deck/.local/lib/python3.10/site-packages/")
 import psutil
+
+# indicate("loaded psutil")
 
 import json
 
@@ -8,7 +22,7 @@ from pprint import pprint
 import sys
 import signal
 import os
-
+import itertools
 
 
 def get_first_match(substr):
@@ -28,18 +42,19 @@ def get_all_steam_apps():
     return res
 
 
+# indicate("got to class")
 class Plugin:
-    async def kill(self, pid):
+    async def kill(self, pid, cmd):
         proc = psutil.Process(pid)
-        sig = signal.SIGSTOP
-        if proc.status() == "stopped":
-            sig = signal.SIGCONT
+        sig = signal.SIGSTOP if cmd == "stop" else signal.SIGCONT
         for child in proc.children(recursive=True):
             os.kill(child.pid, sig)
         os.kill(proc.pid, sig)
 
     async def get_procs(self):
+        # indicate("called get procs")
         apps = get_all_steam_apps()
+        # indicate(str(apps))
         return json.dumps([[app.pid, app.cmdline()[-1], app.status()] for app in apps])
 
     async def _main(self):
